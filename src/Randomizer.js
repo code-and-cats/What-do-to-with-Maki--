dc7c31ef97586c556a7dc669ce486ac6dddbc3e7
Vio Szabo <vio@vioszabo.se>
2025-09-24 14:27:13 +0200

    background & wheel img

---
+++ src/Randomizer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Randomizer() {
+  return (
+    <img
+      src="depositphotos_103734284-stock-photo-wheel-fortune-5-area.png"
+      alt="wheel"
+    />
+  )
+}
+
+export default Randomizer
